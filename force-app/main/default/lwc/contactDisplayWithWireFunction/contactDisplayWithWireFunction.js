42c41938605b0d9f13401df1cbe7b91b7b3da278
Naval Kulkarni <navalkulkarni@gmail.com>
2020-10-25 15:43:12 +0530

    create component to test wire service with function

---
+++ force-app/main/default/lwc/contactDisplayWithWireFunction/contactDisplayWithWireFunction.js
@@ -0,0 +1,33 @@
+import { LightningElement, wire } from 'lwc';
+//import NavigationMixin from 'lightning/navigation';
+import getContacts from '@salesforce/apex/FetchContact.getContacts';
+export default class ContactDisplayWithWireFunction extends LightningElement {
+
+    contacts;
+    errorDetails;
+    contactId;
+
+    @wire(getContacts)
+    checkNameOfAccount({error,data}){
+        if(data){
+            this.contacts = data;
+        }else if(error){
+            this.errorDetails = error;
+        }
+    }
+
+    navigateToContactDetail(event){
+        this.contactId = event.target.value;
+        console.log('in navigate method '+this.contactId);
+        /*
+        this[NavigationMixin.Navigate]({
+            type:'standard__recordPage',
+            attributes:{
+                recordId:this.contactId,
+                objectApiName:'Contact',
+                actionName:'view'
+            }           
+ 
+        }); */ 
+    }
+}
