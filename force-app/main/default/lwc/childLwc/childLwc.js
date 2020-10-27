621953b62908b0d7ce0f2d36bd05f0b62446570a
Naval Kulkarni <navalkulkarni@gmail.com>
2020-10-27 07:41:37 +0530

    add comment for reminding mistake

--- force-app/main/default/lwc/childLwc/childLwc.js
+++ force-app/main/default/lwc/childLwc/childLwc.js
@@ -22,9 +22,12 @@ export default class ChildLwc extends LightningElement {
     callParent(event){
         const value = event.target.value;
         console.log(value);
+        //always remember to write event name in small caps
         const callEvent = new CustomEvent('callparentevent',{
             detail:{value},
         });
+
+        //fire event
         this.dispatchEvent(callEvent);
     }
 
